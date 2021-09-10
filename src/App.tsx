1d4d3448202c3e850bdc1ae55a70688dd913fbc3
ryoma116 <uma19880116>
2021-09-11 05:25:48 +0900

    :sparkles: Add elements for Counter & Name

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+
+const INITIAL_COUNT = 0
+const INITIAL_NAME = "JavaScript"
+
+const SampleComponent = () => {
+    const [count, setCount] = useState(INITIAL_COUNT)
+    const [name, setName] = useState(INITIAL_NAME)
+    const countIncrement = () => setCount((prevCount) => prevCount + 1)
+    const countDecrement = () => setCount((prevCount) => prevCount - 1)
+    const countReset = () => setCount(INITIAL_COUNT)
+
+    const handleChangeName = (e: any) => {
+        setName(e.target.value)
+    }
+
+    return (
+        <div className="App">
+            <p>
+                現在のカウント: <b>{count}</b>
+                <br />
+                count の初期値: <b>{INITIAL_COUNT}</b>
+                <button onClick={countIncrement}>increment</button>
+                <button onClick={countDecrement}>decrement</button>
+                <button onClick={countReset}>reset</button>
+            </p>
+            <p>
+                Hello, <b>{name}</b> !!
+                <br />
+                name の初期値: <b>{INITIAL_NAME}</b>
+            </p>
+
+            <input type="text" onChange={handleChangeName} />
+        </div>
+    )
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Hello! World!
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <SampleComponent />
+            <SampleComponent />
+        </>
+    )
 }
 
 export default App;
